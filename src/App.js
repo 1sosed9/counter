fc10199028102013bcca91d2695efd1aa2d0933a
1sosed9 <vladsosed63@gmail.com>
2023-06-15 22:46:43 +0300

    added functions and project is finished

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
+import React from 'react';
 import './index.scss';
 
 function App() {
+  const [count, setCount] = React.useState(0);
+
+  function onclickPlus() {
+    setCount(count + 1);
+  }
+
+  function onclickMinus() {
+    setCount(count - 1);
+  }
+
   return (
     <div className="App">
       <div>
         <h2>Счетчик:</h2>
-        <h1>0</h1>
-        <button className="minus">- Минус</button>
-        <button className="plus">Плюс +</button>
+        <h1 >{count}</h1>
+        <button className="minus" onClick={onclickMinus}>- Минус</button>
+        <button className="plus" onClick={onclickPlus}>Плюс +</button>
       </div>
     </div>
   );
